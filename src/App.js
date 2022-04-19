a42920341c11dff58d95fd1b076f31501e1f6fb0
Parvesh Narwal <parveshtext@outlook.com>
2022-04-19 17:45:49 +0530

    Fetching data from an API using axios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import HookCounter from "./component/Basics/HookCounter"
 import HookCounterAdvance from "./component/Basics/HookCounterAdvance"
 import HookCounterWithStateAsArray from "./component/Basics/HookCounterWithStateAsArray"
 import HookCounterWithStateAsObject from "./component/Basics/HookCounterWithStateAsObject"
+import DataFetchingUsingAPI from "./component/useEffectHooks/DataFetchingUsingAPI"
 import HookMouse from "./component/useEffectHooks/HookMouse"
 import IntervalHookCounter from "./component/useEffectHooks/IntervalHookCounter"
 import MouseContainer from "./component/useEffectHooks/MouseContainer"
@@ -9,7 +10,7 @@ import MouseContainer from "./component/useEffectHooks/MouseContainer"
 const App = () => {
   return (
     <>
-      <IntervalHookCounter/>
+      <DataFetchingUsingAPI/>
     </>
   )
 }
---
+++ src/component/useEffectHooks/DataFetchingUsingAPI.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const DataFetchingUsingAPI = () => {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response)
+                setPosts(response.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+
+    return (
+        <div>
+            <ul>
+                {
+                    posts.map(post => {
+                        return <li key={post.id}>{post.title}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default DataFetchingUsingAPI
